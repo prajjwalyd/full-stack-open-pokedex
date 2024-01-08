367ae8115286168c95acc6bb1be5a915f2a6a863
Prajjwal <prajjwalyd@gmail.com>
2024-01-09 03:22:38 +0530

    updating build step

--- build_step.sh
+++ build_step.sh
@@ -1,11 +1,12 @@
-#!/bin/bash
+# Install NVM
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
 
-echo "Build script"
+# Activate NVM
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 
-# add the commands here
-
-nvm install 16
-nvm use 16
-
+# Use Node.js version specified in .nvmrc
+nvm install
+nvm use
 npm install
 npm run build
